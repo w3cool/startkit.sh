77dab67a01429514ac1dc64bdb23debf9c62656d
weijie <zhangweijie@gmail.com>
2020-12-02 23:10:25 +0800

    fixbug

--- centos8/centos8-init-k8s-node.sh
+++ centos8/centos8-init-k8s-node.sh
@@ -5,16 +5,16 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl --system
 
-echo "=========install kuberctl========================="
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+echo "=========install kubernetes repo======================"
+#cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+#[kubernetes]
+#name=Kubernetes
+#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+#enabled=1
+#gpgcheck=1
+#repo_gpgcheck=1
+#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+#EOF
 
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -26,8 +26,12 @@ repo_gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
-dnf install -y kubectl
+echo "=========install kubernetes cli tools (kubelet kubeadm kubectl)======================"
 dnf install bash-completion -y
+# 将 SELinux 设置为 permissive 模式（相当于将其禁用）
+#sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable --now kubelet
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube
@@ -40,23 +44,19 @@ minikube start --vm-driver= --image-repository=registry.cn-hangzhou.aliyuncs.com
 echo "=========install kubeadm========================="
 curl -s https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg > rpm-package-key.gpg 
 rpm --import rpm-package-key.gpg 
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
-EOF
+
+
+
 setenforce 0
-dnf install -y kubelet kubeadm kubectl
+#dnf install -y kubelet kubeadm kubectl
 systemctl enable kubelet && systemctl start kubelet
 
 firewall-cmd --permanent --add-port=443/tcp
 firewall-cmd --permanent --add-port=8443/tcp
 firewall-cmd --permanent --add-port=6443/tcp
-firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10250-10252/tcp
+
+firewall-cmd --permanent --add-port=30000-32767/tcp
 firewall-cmd --add-masquerade --permanent 
 firewall-cmd --reload
 
