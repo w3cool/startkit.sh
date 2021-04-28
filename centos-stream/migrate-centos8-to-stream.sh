515af60648c58fdd03d8a849f840080c10a1abd2
w3cool <zhangweijie@gmail.com>
2021-04-28 09:29:35 +0800

    fixbug

--- centos-stream/migrate-centos8-to-stream.sh
+++ centos-stream/migrate-centos8-to-stream.sh
@@ -2,6 +2,7 @@
 #The CentOS Project announced today an end to a classic CentOS Linux as you know it, ending an era of CentOS as a RHEL rebuild. CentOS 8 will continue as a Stream version that you should upgrade to before the end of 2021. 
 #
 dnf makecache
+dnf update -y
 sudo dnf install centos-release-stream -y
 
 dnf swap -y centos-{linux,stream}-repos
